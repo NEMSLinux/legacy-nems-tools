c8cf46262468a1e7604ad1456e03a9c47c7068a6
NEMS Tools <nems@category5.tv>
2020-12-02 01:29:30 +0000

    Add some polish

--- www/log.php
+++ www/log.php
@@ -1,7 +1,7 @@
 <?php
   if (file_exists('/var/log/gpioe.log')) {
     $log = file_get_contents('/var/log/gpioe.log');
-    echo str_replace(array('. ','Pin ','Iterat','Omzlo'),array('.<br />','<br />Pin ','<br />Iterat','<br />Omzlo'),$log);
+    echo str_replace(array('"','. ','Pin ','Iterat','Omzlo'),array('','.<br />','<br />Pin ','<br />Iterat','<br />Omzlo'),$log);
   } else {
     echo 'No connection to NEMS Linux...';
   }
